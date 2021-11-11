4420014fef8dedc1e9435ca77e70ab7e9d805a30
KENKEN0803 <blackflamekr@gmail.com>
2021-11-11 22:15:00 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import LoginForm from "./components/Common/LoginForm";
-import RegisterFormM from "./components/Common/RegisterFormM";
 
 function App() {
     return (
