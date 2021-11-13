d0339fc23d3628f21da8a592c592ffb3a68be822
KENKEN0803 <blackflamekr@gmail.com>
2021-11-13 20:19:27 +0900

    가게 헤더 추가

--- src/components/Owner/OwnerMain.tsx
+++ src/components/Owner/OwnerMain.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function OwnerMain() {
+
+//     const AppBlock = styled.div`
+//   width: 512px;
+//   margin: 0 auto;
+//   margin-top: 4rem;
+//   border: 1px solid black;
+//   padding: 1rem;
+// `;
+
     return (
         <>
-            <h1>업주 메인 페이지</h1>
+            <div>업주 메인 페이지</div>
         </>
     )
 }
