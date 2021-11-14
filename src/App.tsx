d82323184b15efefa7f0d3c35cadc3db99feed5a
dudwls915@naver.com <ghqkr913!>
2021-11-14 17:47:50 +0900

    Product 상품등록-1

--- src/App.tsx
+++ src/App.tsx
@@ -20,29 +20,29 @@ import Footer from "./components/Common/Footer";
 function App() {
 
 
-    return (
-        <>
-            <Header/>
-            {/*<Switch>*/}
-            <Route exact path='/master' component={MasterMain}/>
+        return (
+            <>
+                    <Header/>
+                    {/*<Switch>*/}
+                    <Route exact path='/master' component={MasterMain}/>
 
-            <Route path='/login' component={LoginForm}/>
+                    <Route path='/login' component={LoginForm}/>
 
-            <Route exact path='/user' component={UserMain}/>
-            <Route path='/user/register' component={UserRegisterForm}/>
+                    <Route exact path='/user' component={UserMain}/>
+                    <Route path='/user/register' component={UserRegisterForm}/>
 
-            <Route path='/owner' component={OwnerNavbar}/>
-            <Route exact path='/owner' component={OwnerMain}/>
-            <Route path='/owner/register' component={OwnerRegisterForm}/>
-            <Route path='/owner/addproduct' component={AddProduct}/>
-            <Route path='/owner/goodsview' component={GoodsView}/>
-            <Route path='/owner/reservationview' component={ReservationView}/>
-            <Route path='/owner/sellingview' component={SellingView}/>
-            <Route path='/owner/unsubscribe' component={Unsubscribe}/>
-            {/*</Switch>*/}
-            <Footer/>
-        </>
-    );
+                    <Route path='/owner' component={OwnerNavbar}/>
+                    <Route exact path='/owner' component={OwnerMain}/>
+                    <Route path='/owner/register' component={OwnerRegisterForm}/>
+                    <Route path='/owner/addproduct' component={AddProduct}/>
+                    <Route path='/owner/goodsview' component={GoodsView}/>
+                    <Route path='/owner/reservationview' component={ReservationView}/>
+                    <Route path='/owner/sellingview' component={SellingView}/>
+                    <Route path='/owner/unsubscribe' component={Unsubscribe}/>
+                    {/*</Switch>*/}
+                    <Footer/>
+            </>
+        );
 }
 
 export default App;
