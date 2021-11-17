ced5215ff7e3c1900e1bd5100333745b6b56c199
KENKEN0803 <blackflamekr@gmail.com>
2021-11-17 16:59:11 +0900

    버그수정

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -6,6 +6,7 @@ import {Link, useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import logo from '../../logo.svg';
 import {RootState} from "../../index";
+import '../../styles/test.css';
 
 export default function Header() {
 
@@ -21,6 +22,9 @@ export default function Header() {
         history.push('/');
     };
 
+
+
+
     return (
         <>
             <Navbar bg="dark" variant="dark">
@@ -28,11 +32,11 @@ export default function Header() {
                     <img src={logo} style={{height: '30px'}} alt="logo"/>
                     <Navbar.Brand>공통헤더(임시)</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link as={Nav}><Link to={'/'}>메인페이지</Link></Nav.Link>
-                        <Nav.Link as={Nav}><Link to={'/login'}>로그인</Link></Nav.Link>
-                        <Nav.Link as={Nav}><Link to={'/user'}>유저메인</Link></Nav.Link>
-                        <Nav.Link as={Nav}><Link to={'/owner'}>오너메인</Link></Nav.Link>
-                        <Nav.Link as={Nav}><Link to={'/master'}>마스터메인</Link></Nav.Link>
+                        <Nav.Link as={Nav}><Link className={'youngJin'} to={'/'}>메인페이지</Link></Nav.Link>
+                        <Nav.Link as={Nav}><Link className={'youngJin'} to={'/login'}>로그인</Link></Nav.Link>
+                        <Nav.Link as={Nav}><Link className={'youngJin'} to={'/user'}>유저메인</Link></Nav.Link>
+                        <Nav.Link as={Nav}><Link className={'youngJin'} to={'/owner'}>오너메인</Link></Nav.Link>
+                        <Nav.Link as={Nav}><Link className={'youngJin'} to={'/master'}>마스터메인</Link></Nav.Link>
                     </Nav>
 
                     <span style={{color: 'white'}}>
