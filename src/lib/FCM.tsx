bcceb3521c294a081d2fb959bc5179f30c15e417
KENKEN0803 <blackflamekr@gmail.com>
2021-11-26 19:30:18 +0900

    주문시 일자 입력란 추가

--- src/lib/FCM.tsx
+++ src/lib/FCM.tsx
@@ -10,7 +10,13 @@ import Button from '@mui/material/Button';
 export default function FCM() {
     const [show, setShow] = useState(false);
     const [isTokenFound, setTokenFound] = useState(false);
-    const [notification, setNotification] = useState<{ title?: string, body?: string }>({title: '', body: ''});
+    const [notification, setNotification] = useState<{ title?: string, body?: string, etc?: string, img?: string, }>
+    ({
+        title: '',
+        body: '',
+        etc: '',
+        img: '',
+    });
 
     const firebaseConfig = {
         apiKey: "AIzaSyCdDnZLLw2wo1KpixiGqQKXKKOES0pi1bU",
