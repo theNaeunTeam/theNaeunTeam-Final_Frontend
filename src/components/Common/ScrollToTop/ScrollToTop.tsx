8c1eed727c501e10926a835dbc7fafcc43b6a64d
KENKEN0803 <blackflamekr@gmail.com>
2021-12-03 10:19:09 +0900

    위로 가기 버튼 추가

--- src/components/Common/ScrollToTop/ScrollToTop.tsx
+++ src/components/Common/ScrollToTop/ScrollToTop.tsx
@@ -3,14 +3,13 @@ import './ScrollToTop.scss';
 
 export default function ScrollToTop() {
 
-
     const [ScrollY, setScrollY] = useState(0);
     const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
 
     const handleFollow = () => {
         setScrollY(window.pageYOffset);
-        if (ScrollY > 100) {
-            // 100 이상이면 버튼이 보이게
+        if (ScrollY > 500) {
+            // 500 이상이면 버튼이 보이게
             setBtnStatus(true);
         } else {
             // 100 이하면 버튼이 사라지게
